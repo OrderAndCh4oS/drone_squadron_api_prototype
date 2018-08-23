c6283db839b87a15b17381969030c0dcd8a964fa
sarcoma <sarcoma@live.co.uk>
2018-08-23 16:05:37 +0100

    Made sqlite path relative

--- drone_squadron/database/engine.py
+++ drone_squadron/database/engine.py
@@ -3,6 +3,6 @@ import os
 from sqlalchemy import create_engine
 
 if "PYTEST" in os.environ:
-    engine = create_engine('sqlite:////home/sarcoma/PycharmProjects/drone_squadron/tests/drones_test.db')
+    engine = create_engine('sqlite:///drones_test.db')
 else:
-    engine = create_engine('sqlite:////home/sarcoma/PycharmProjects/drone_squadron/drone_squadron/drones.db')
+    engine = create_engine('sqlite:///drones.db')
