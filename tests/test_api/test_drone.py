cc2ea7360cee058c41993279670198c3bc1a9ab7
sarcoma <sarcoma@live.co.uk>
2018-10-15 18:12:15 +0100

    Should throw error when squadron does not have enough scrap to make purchase

--- tests/test_api/test_drone.py
+++ tests/test_api/test_drone.py
@@ -1,9 +1,18 @@
+from sqlalchemy.engine import ResultProxy
+
+from crud.drone_crud import DroneCrud
+from crud.squadron_crud import SquadronCrud
+
+
 class TestDrone:
 
     def test_post(self, log_in, test_client):
+        with SquadronCrud() as crud:
+            result = crud.insert(name="Squad Test", user=1)  # type: ResultProxy
+            last_id = result.inserted_primary_key[0]
         response = test_client.post(
             '/drone',
-            json={"name": "Drone One", "squadron": 1},
+            json={"name": "Drone One", "squadron": last_id},
             content_type="application/json"
         )
         data = response.get_json()
@@ -28,10 +37,19 @@ class TestDrone:
         assert response.status_code == 401
 
     def test_get(self, log_in, test_client):
-        response = test_client.get('/drone/1')
+        with SquadronCrud() as crud:
+            result = crud.insert(name="Squad Test", user=1)  # type: ResultProxy
+            last_squadron_id = result.inserted_primary_key[0]
+        with DroneCrud() as crud:
+            result = crud.insert(
+                name="Drone One",
+                squadron=last_squadron_id,
+            )  # type: ResultProxy  # type: ResultProxy
+            last_drone_id = result.inserted_primary_key[0]
+        response = test_client.get('/drone/%d' % last_drone_id)
         data = response.get_json()
         assert response.status_code == 200
-        assert 1 == data['id']
+        assert last_drone_id == data['id']
         assert 'id' in data
         assert 'name' in data
         assert 'kills' in data
@@ -42,7 +60,6 @@ class TestDrone:
         assert 'thruster_name' in data
         assert 'steering_name' in data
         assert 'scanner_name' in data
-
 
     def test_get_by_squadron(self, log_in, test_client):
         response = test_client.get('/squadron/1/drone')
@@ -59,7 +76,6 @@ class TestDrone:
         assert 'thruster_name' in data
         assert 'steering_name' in data
         assert 'scanner_name' in data
-
 
     def test_put(self, log_in, test_client):
         response = test_client.put(
