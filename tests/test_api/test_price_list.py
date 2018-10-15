899d1d868510037df934ea35fa98a3a15df107c3
sarcoma <sarcoma@live.co.uk>
2018-10-15 11:00:50 +0100

    Tests passing, but could be more thorough

--- tests/test_api/test_price_list.py
+++ tests/test_api/test_price_list.py
@@ -4,6 +4,4 @@ class TestPriceList:
         response = test_client.get('/price-list')
         data = response.get_json()[0]
         assert response.status_code == 200
-        assert 'round_type' == data['item']
-        assert 1 == data['related_id']
-        assert 0 == data['scrap']
+        assert 'weapon' == data['item']
